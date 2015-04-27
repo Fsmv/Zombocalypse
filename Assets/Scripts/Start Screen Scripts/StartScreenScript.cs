d27297bd7e82b73f58b0902dfb0d2f3b1b1453b9
Jacob McCanty <mccantjt@muohio.edu>
2015-04-27 16:59:33 -0400

    Added level change script and scenes

---
+++ Assets/Scripts/Start Screen Scripts/StartScreenScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+
+	void OnGUI () {
+		if(GUI.Button( new Rect( Screen.width/2 - 90,Screen.height/2 + 175, 180, 65),"Start the Game")) {
+			Application.LoadLevel(1);
+		}
+	} // end OnGUI
+}
