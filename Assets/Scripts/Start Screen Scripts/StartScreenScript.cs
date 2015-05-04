8ffd3836a6da567d43f4d908d7f27085e2243bd4
Mark Kuczmarski <kuczmama@miamiOH.edu>
2015-05-04 19:53:49 -0400

    Revert "Merge branch 'master' of https://github.com/Fsmv/487-final-project"
    This reverts commit 1ba62c93bdeb23146fd16aab89fd3de0a8b9f201, reversing
    changes made to 675d806fc4f730205673b4f56abd6169ea8ea0d8.

--- Assets/Scripts/Start Screen Scripts/StartScreenScript.cs
+++ Assets/Scripts/Start Screen Scripts/StartScreenScript.cs
@@ -3,23 +3,8 @@ using System.Collections;
 
 public class StartScreenScript : MonoBehaviour {
 
-	public Texture background;
-	private bool showMenu=false;
-
 	void OnGUI () {
-		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height) , background);
-
-		if (showMenu == true) {
-			GUI.BeginGroup(new Rect(Screen.width/2-50,Screen.height/2 -45,100,90));
-
-			GUI.Box(new Rect(0,0,100,90), "Credits");
-		}
-
-		if (GUI.Button (new Rect ((Screen.width*2) / 3 - 90, Screen.height / 2 + 175, 180, 65), "Credits")) {
-			showMenu=!showMenu;
-		}
-
-		if(GUI.Button( new Rect( Screen.width/3 - 90,Screen.height/2 + 175, 180, 65),"Start the Game")) {
+		if(GUI.Button( new Rect( Screen.width/2 - 90,Screen.height/2 + 175, 180, 65),"Start the Game")) {
 			Application.LoadLevel(1);
 		}
 	} // end OnGUI
