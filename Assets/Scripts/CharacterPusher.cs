a0e86202d1f652e02f4320879e0257e4469fa63a
Andrew Kallmeyer <fsmv@sapium.net>
2015-05-04 17:02:12 -0400

    Added swinging door

---
+++ Assets/Scripts/CharacterPusher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterPusher : MonoBehaviour {
+	public float pushForce;
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Rigidbody hitRigid = hit.gameObject.GetComponent<Rigidbody> ();
+		if (hitRigid != null) {
+			hitRigid.AddForce (hit.moveDirection * pushForce);
+		}
+	}
+}
