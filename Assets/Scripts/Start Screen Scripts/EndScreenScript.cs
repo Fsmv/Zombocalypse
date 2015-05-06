bae06eeb179e853186d6f7d133f75b25baf75d0a
unknown <mccantjt@EAS-BEN002-B5.it.muohio.edu>
2015-05-06 16:27:21 -0400

    Re-added start and end screen stuff

--- Assets/Scripts/Start Screen Scripts/EndScreenScript.cs
+++ Assets/Scripts/Start Screen Scripts/EndScreenScript.cs
@@ -2,8 +2,11 @@
 using System.Collections;
 
 public class EndScreenScript : MonoBehaviour {
-
+	
+	public Texture background;
+	
 	void OnGUI () {
+		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height) , background);
 		if(GUI.Button( new Rect( Screen.width/2 - 90,Screen.height/2 + 175, 180, 65),"Restart the Game")) {
 			Application.LoadLevel(0);
 		}
