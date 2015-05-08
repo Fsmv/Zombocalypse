26b78a89da861e8db015e3cbb4c60824e508234a
Jacob McCanty <mccantjt@muohio.edu>
2015-05-08 18:22:23 -0400

    Added in the credits and links
    Also ensured levels transition appropriately and added a title to our
    start screen, feel free to change it though. I will work on the end
    screen and score a little later

--- Assets/Scripts/Start Screen Scripts/StartScreenScript.cs
+++ Assets/Scripts/Start Screen Scripts/StartScreenScript.cs
@@ -3,26 +3,10 @@ using System.Collections;
 
 public class StartScreenScript : MonoBehaviour {
 	
-	public Texture background;
-	private bool showMenu=false;
-	
 	void OnGUI () {
-		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height) , background);
-		
-		if (showMenu == true) {
-			GUI.BeginGroup(new Rect(Screen.width/2-50,Screen.height/2 -45,100,90));
-			
-			GUI.Box(new Rect(0,0,100,90), "Credits");
-
-			if (GUI.Button (new Rect ((Screen.width*2) / 3 - 90, Screen.height / 2 + 175, 180, 65), "Main Menu")) {
-				showMenu=!showMenu;
-			}
-
-			GUI.EndGroup();
-		}
 		
 		if (GUI.Button (new Rect ((Screen.width*2) / 3 - 90, Screen.height / 2 + 175, 180, 65), "Credits")) {
-			showMenu=!showMenu;
+			Application.LoadLevel(3);
 		}
 		
 		if(GUI.Button( new Rect( Screen.width/3 - 90,Screen.height/2 + 175, 180, 65),"Start the Game")) {
